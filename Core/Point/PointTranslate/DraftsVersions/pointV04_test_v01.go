33f7aab7ba39b54f8a116191b3525cb424a93baa
K Flora <56885337+KiloLimaFoxtrot@users.noreply.github.com>
2020-09-02 01:27:15 -0700

    Update pointV04_test_v01.go

--- Core/Point/PointTranslate/DraftsVersions/pointV04_test_v01.go
+++ Core/Point/PointTranslate/DraftsVersions/pointV04_test_v01.go
@@ -1,4 +1,5 @@
 package main
+
 /*
 import (
 	"fmt"
@@ -10,38 +11,38 @@ import (
 // }
 
 func TestNew(t *testing.T) {
-	
+
 	fmt.Println()
 	fmt.Println("[*** pointV04 Point Translation Tests ***]")
-	
+
 	// Test Structs
 	type PointStrct struct {
 		x int64
 		y int64
 	}
-	
+
 	type SGFStrct struct {
 		sgfX string
 		sgfY string
 	}
-	
+
 	const aValue = int64('a')
-	
+
 	// Correct results
 	// PntCrct01 := New(8, 20)
 	// PntCrct02 := New(13, 5)
-	
+
 	// Point Creation Test
 	var x1 int64 = 8
 	var y1 int64 = 20
-	
+
 	// Test Control Point
 	PntCrct01 := PointStrct{
 		x: x1,
 		y: y1,
 	}
 	Pnt01 := New(int64(x), int64(y))
-	
+
 	fmt.Println()
 	if Pnt01 != PntCrct01 {
 		fmt.Println()
@@ -57,7 +58,7 @@ func TestNew(t *testing.T) {
 		string(rune((PntCrct01.x) + aValue)),
 		string(rune((PntCrct01.y) + aValue)),
 	}
-	
+
 	SGF01 := Pnt01.ToSGF()
 	fmt.Println()
 	if SGF01 != SGFCrct01 {
@@ -66,7 +67,7 @@ func TestNew(t *testing.T) {
 			"got: %d\n"+
 			"want: &d \n", SGF01, SGFCrct01)
 	}
-	
+
 	Pnt02 := NewFromSGF(SGF01)
 	fmt.Println()
 	if Pnt02 != PntCrct01 {
@@ -75,7 +76,7 @@ func TestNew(t *testing.T) {
 			"got: %d \n"+
 			"want: &d \n", Pnt02, PntCrct01)
 	}
-	
+
 }*/
 
 /*
